3c74c0d73f24ed8be05e9ef8d1a9f39bba633ca2
Oluwajoba <oluwajobabello@gmail.com>
2020-06-06 12:51:38 +0100

    recovery6

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     .map((x, i) => console.log(x + " joba", i));
 
   return (
+<<<<<<< HEAD
     <>
       {!data.data ? (
         "isLoading"
@@ -48,6 +49,25 @@ function App() {
         </div>
       )}
     </>
+=======
+    <div className="App">
+      <h1>Weather App</h1>
+      {console.log("res: ", data)}
+      {console.log("res2: ", data.data)}
+      location: {`${data.data.name}, ${data.data.sys.country}`} <br />
+      coordinates:{" "}
+      {`{lon: ${data.data.coord.lon}, lat: ${data.data.coord.lat}}`} <br />
+      Timezone: {`${data.data.timezone}`} <br />
+      WeatherDiscription: {`${data.data.weather[0].description}`} <br />
+      Temperature: {`${data.data.main.temp}`} <br />
+      Min-Temp: {`${data.data.main.temp_min}`} <br />
+      Max-Temp: {`${data.data.main.temp_max}`} <br />
+      Feels Like: {`${data.data.main.feels_like}`} <br />
+      Pressure: {`${data.data.main.pressure}`} <br />
+      Humidity: {`${data.data.main.humidity}`} <br />
+      Wind Speed: {`${data.data.wind.speed}`} <br />
+    </div>
+>>>>>>> parent of bcca15e... add tenary condition to handle handle undefine before component mounts / App.js
   );
 }
 
