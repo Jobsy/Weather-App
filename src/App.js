51d9d68fa0d02490f37ba1c86f386233de2e5f82
Oluwajoba <oluwajobabello@gmail.com>
2020-06-06 13:58:48 +0100

    recovery

--- src/App.js
+++ src/App.js
@@ -45,8 +45,34 @@ function App() {
       ) : (
         <div className="App">
           <h1>Weather App</h1>
-          {console.log("query: ", query.toString().split(","))}
-          <AppBody data={data} />
+          {console.log("res: ", data)}
+          {console.log("res2: ", data.data)}
+          location: {`${data.data.name}, ${data.data.sys.country}`} <br />
+          coordinates:{" "}
+          {`{lon: ${data.data.coord.lon}, lat: ${data.data.coord.lat}}`} <br />
+          Timezone: {`GMT ${data.data.timezone / 3600}`} <br />
+          WeatherDiscription: {`${data.data.weather[0].description}`} <br />
+          Sunrise:{" "}
+          {` ${new Date(
+            data.data.sys.sunrise * 1000
+          ).toLocaleTimeString()} `}{" "}
+          <br />
+          Sunset:{" "}
+          {` ${new Date(
+            data.data.sys.sunset * 1000
+          ).toLocaleTimeString()} `}{" "}
+          <br />
+          Temperature: {`${data.data.main.temp} \xB0C`} <br />
+          Min-Temp: {`${data.data.main.temp_min} \xB0C`} <br />
+          Max-Temp: {`${data.data.main.temp_max} \xB0C`} <br />
+          Feels Like: {`${data.data.main.feels_like} \xB0C`} <br />
+          Pressure: {`${data.data.main.pressure} hPa`} <br />
+          Humidity: {`${data.data.main.humidity} %`} <br />
+          Wind Speed: {`${data.data.wind.speed} m/s`} <br />
+          Wind Degree: {`${data.data.wind.deg} \xB0`} <br />
+          <img
+            src={`http://openweathermap.org/img/w/${data.data.weather[0].icon}.png`}
+          />
         </div>
       )}
     </>
