c00934d34b9e30214fc13ead3d15ce74ebf0a01b
Oluwajoba <oluwajobabello@gmail.com>
2020-06-09 10:48:02 +0100

    create a new components foloder / move all components to it /  AppBody.js, Fetch.js, &  Query.js

---
+++ src/components/AppBody.js
@@ -0,0 +1,117 @@
+import React from "react";
+import { Card, Icon, Image, Label, Reveal } from "semantic-ui-react";
+
+export const AppBody = (props) => (
+  <div className="appBody">
+    <Reveal animated="move" instant>
+      <Reveal.Content visible>
+        <Card>
+          <Image
+            src={`http://openweathermap.org/img/w/${props.data.data.weather[0].icon}.png`}
+            wrapped
+            ui={false}
+          />
+
+          <Card.Content>
+            <Card.Header>
+              {`${props.data.data.name}, ${props.data.data.sys.country}`}
+            </Card.Header>
+            <div className="header1">
+              <Card.Meta>
+                <span className="date">
+                  <Icon name="compass outline" color="grey" />
+                  {` {lon: ${props.data.data.coord.lon}, lat: ${props.data.data.coord.lat}}`}{" "}
+                </span>
+              </Card.Meta>
+              <Card.Meta>
+                <span className="date">{`GMT ${
+                  props.data.data.timezone / 3600
+                }`}</span>
+              </Card.Meta>
+            </div>
+          </Card.Content>
+
+          <Card.Content>
+            <Label>Weather</Label>
+            <br />
+            <Card.Description>
+              {`Main:  ${props.data.data.weather[0].main}`}
+              <br />
+            </Card.Description>
+            <Card.Description>
+              {`Description: ${props.data.data.weather[0].description}`}
+              <br />
+            </Card.Description>
+            <Card.Description>
+              {`Visibility: ${props.data.data.visibility} mm`} <br />
+            </Card.Description>
+          </Card.Content>
+
+          <Card.Content>
+            <Label>Wind</Label>
+            <Card.Description>
+              <div className="header1">
+                <span className="date">
+                  {`Speed: ${props.data.data.wind.speed} m/s`}
+                </span>
+
+                <span className="date">
+                  {`Degree: ${props.data.data.wind.deg} \xB0C`}
+                </span>
+              </div>
+            </Card.Description>
+          </Card.Content>
+        </Card>
+      </Reveal.Content>
+      <Reveal.Content hidden>
+        <Card>
+          <Card.Content extra>
+            <div className="header1">
+              <span>
+                <Icon name="sun outline" color="yellow" />
+                {` ${new Date(
+                  props.data.data.sys.sunrise * 1000
+                ).toLocaleTimeString()} `}
+              </span>
+              <span>
+                {` ${new Date(
+                  props.data.data.sys.sunset * 1000
+                ).toLocaleTimeString()} `}
+                <Icon name="sun" color="orange" />
+              </span>
+            </div>
+          </Card.Content>
+
+          <Card.Content extra>
+            <Card.Meta>
+              <Icon name="thermometer empty" color="yellow" />
+              {`Min-Temp: ${props.data.data.main.temp_min} \xB0C`}
+            </Card.Meta>
+            <Card.Meta>
+              <Icon name="thermometer half" color="green" />
+              {`Normal: ${props.data.data.main.temp} \xB0C`}
+            </Card.Meta>
+            <Card.Meta>
+              <Icon name="thermometer full" color="red" />
+              {`Max-Temp: ${props.data.data.main.temp_max} \xB0C`}
+            </Card.Meta>
+          </Card.Content>
+
+          <Card.Content extra>
+            <p>
+              <Icon name="weight" color="violet" />
+              {`Pressure: ${props.data.data.main.pressure} hPa`}
+            </p>
+          </Card.Content>
+
+          <Card.Content extra>
+            <p>
+              <Icon name="tint" color="brown" />
+              {`Humidity: ${props.data.data.main.humidity} %`} <br />
+            </p>
+          </Card.Content>
+        </Card>
+      </Reveal.Content>
+    </Reveal>
+  </div>
+);
---
+++ src/components/Query.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Fetch } from "../components/Fetch";
+
+export const Query = () => {
+  const [query, setQuery] = useState([]);
+
+  const setLocation = () => {
+    setQuery(document.getElementById("location").value);
+  };
+
+  const showSubmitButton = (e) => {
+    const submit = document.querySelector(".submitContainer");
+    if (e.target.value) {
+      submit.style.opacity = "1";
+    } else {
+      submit.style.opacity = "0";
+    }
+  };
+
+  return (
+    <div className="mainGroup">
+      <div className="group1">
+        <h1>Weather App</h1>
+        <div className="inputContainer">
+          <div className="input">
+            <div className="inputContainer2">
+              <input
+                type="email"
+                className="inputField"
+                id="location"
+                placeholder="Enter a location(state) or comma separated locations"
+                onChange={showSubmitButton}
+              />
+              <div className="inputField-shadow"></div>
+            </div>
+            <div className="submitContainer">
+              <input
+                type="submit"
+                className="submit-btn"
+                onClick={setLocation}
+              />
+              <div className="submit-btn-shadow"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="group2">
+        {query
+          .toString()
+          .split(",")
+          .map((locations, index) => (
+            <div key={index}>
+              <Fetch locations={locations} />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
